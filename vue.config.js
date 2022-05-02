a441cd141694f196f976f84c3bb3512fe007e70c
nks <nks@ulalalab.com>
2022-05-02 14:23:56 +0900

    proxy 제거 / cors server 추가

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,4 @@
 module.exports = {
     outputDir: 'docs',
     publicPath : '/',
-    devServer: {
-        proxy: {
-            '/1790387': {
-                "target":"http://apis.data.go.kr",
-                "changeOrigin":true,
-                "secure":false
-            },
-        }
-    }
 }
